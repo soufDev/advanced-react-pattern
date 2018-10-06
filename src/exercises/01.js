7abb9dd0f03b0840639def58f986f223db44e5c9
Soufiane AIT AKKACHE <ait.akkache.soufiane@gmail.com>
2018-10-06 17:35:36 +0200

    finish the exercise 01

--- src/exercises/01.js
+++ src/exercises/01.js
@@ -3,9 +3,20 @@
 import React from 'react'
 // 🐨 uncomment this import to get the switch component.
 // It takes an `onClick` and an `on` prop
-// import {Switch} from '../switch'
+import {Switch} from '../switch'
 
 class Toggle extends React.Component {
+  state = {
+    on: false,
+  }
+
+  toggle = () => {
+    this.setState(
+      ({on}) => ({on: !on}),
+      () => {
+        this.props.onToggle(this.state.on);
+      });
+  }
   // 🐨 this toggle component is going to need to have state for `on`
   //
   // You'll also want a method to handle when the switch is clicked
@@ -18,8 +29,9 @@ class Toggle extends React.Component {
   //
   // 💯 Use a state updater function for `newState` to avoid issues with batching
   render() {
+    const { on } = this.state;
     // 🐨 here you'll want to return the switch with the `on` and `onClick` props
-    return null
+    return <Switch onClick={this.toggle} on={on} />
   }
 }
 
