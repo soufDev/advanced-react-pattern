7276fb09919ae2c1a6d24a03786c6e90792ae24e
Soufiane AIT AKKACHE <ait.akkache.soufiane@gmail.com>
2018-10-06 19:43:23 +0200

    exercise 02 finished :D

--- src/__tests__/02.js
+++ src/__tests__/02.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {renderToggle} from '../../test/utils'
-import Usage from '../exercises-final/02'
-// import Usage from '../exercises/02'
+// import Usage from '../exercises-final/02'
+import Usage from '../exercises/02'
 
 test('renders a toggle component', () => {
   const handleToggle = jest.fn()
@@ -28,8 +28,8 @@ test('renders a toggle component', () => {
 /*
 http://ws.kcd.im/?ws=react%20patterns&e=02&em=ait.akkache.soufiane@gmail.com
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
