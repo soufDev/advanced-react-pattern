73b74bd77b0806257092fa127270e637a1d2d9c6
Soufiane AIT AKKACHE <ait.akkache.soufiane@gmail.com>
2018-10-07 17:40:10 +0200

    finish the Prop Getters exercise 06

--- src/__tests__/06.js
+++ src/__tests__/06.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {renderToggle, fireEvent} from '../../test/utils'
-import Usage from '../exercises-final/06'
-// import Usage from '../exercises/06'
+// import Usage from '../exercises-final/06'
+import Usage from '../exercises/06'
 
 test('renders a toggle component', () => {
   const handleToggle = jest.fn()
@@ -56,8 +56,8 @@ test('passes custom props to the custom-button', () => {
 /*
 http://ws.kcd.im/?ws=react%20patterns&e=06&em=ait.akkache.soufiane@gmail.com
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
